ca00d80f161bc7c328d9123c61482b81483a3da5
Shiva Dhakal <theskdhakal@gmail.com>
2025-05-05 22:05:40 +1000

    core url added

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Image(models.Model):
+    title=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='uploads/')
+    uploaded_by=models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
