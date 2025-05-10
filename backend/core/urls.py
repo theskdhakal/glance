933ead793ce587f6a135bf68db3c266adc85963f
Shiva Dhakal <theskdhakal@gmail.com>
2025-05-10 13:36:38 +1000

    frontend started

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from .views import ImageListCreate, ToggleLike
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 
 
@@ -6,5 +11,9 @@ app_name='core'
 
 
 urlpatterns = [
-    
+    path('images/', ImageListCreate.as_view()),
+    path('images/<int:pk>/like/', ToggleLike.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+
 ]
---
+++ frontend/my-app/src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-red-500 text-white p-4 flex justify-center align-center">
+        <h1> Hello there, i am back</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
