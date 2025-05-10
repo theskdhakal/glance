6fc1cc81a642a8922300b00a02e6347fe31399e6
Shiva Dhakal <theskdhakal@gmail.com>
2025-05-10 10:41:00 +1000

    serializers class written for all models

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,1 +1,24 @@
 from rest_framework import serializers
+from .models import Image, Like
+from django.contrib.auth.models import User
+
+class UserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['id', 'username']
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Like
+        fields='__all__'
+
+class ImageSerializer(serializers.ModelSerializer):
+    uploaded_by=UserSerializers(read_only=True)
+    likes=serializers.SerializerMethodField()
+
+    class Meta:
+        model=Image
+        fields=['id','title','image','uploaded_by','created_at','likes']
+
+    def get_likes(self, obj):
+        return obj.likes.count()
