eca355a7b03ab9bcd9e055747487828d718a7077
Shiva Dhakal <theskdhakal@gmail.com>
2025-05-27 10:45:34 +1000

    fix:import path

--- frontend/my-app/src/main.tsx
+++ frontend/my-app/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from '../store.ts'
+import { store } from '../store.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
